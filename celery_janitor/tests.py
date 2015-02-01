35bc8d1d262d658dc1d75d20cc46f853245c4d2f
Andrei Coman <andrei.coman@3pillarglobal.com>
2015-02-01 11:54:52 +0200

    Add test

---
+++ celery_janitor/tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+import mock
+
+from celery_janitor.utils import Config
+
+
+class ConfigTest(unittest.TestCase):
+
+    @mock.patch('celery_janitor.utils.conf.BROKER_URL', 'sqs://aws_access_key_id:aws_secret@')
+    def test_sqs_backend(self):
+        self.config = Config()
+        backend = self.config.get_backend()
+        self.assertEqual(backend, 'celery_janitor.backends.sqs.SQSBackend')
+        key, secret = self.config.get_credentials()
+        self.assertEqual(key, 'aws_access_key_id')
+        self.assertEqual(secret, 'aws_secret')
